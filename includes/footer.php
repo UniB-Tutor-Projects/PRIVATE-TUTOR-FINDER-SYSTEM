6ee3498cf56ddfd8d9721c6fb53701222c3d625f
JuniorDCoder <119924067+JuniorDCoder@users.noreply.github.com>
2023-03-14 11:07:55 +0100

    Navigation and footer files

---
+++ includes/footer.php
@@ -0,0 +1,117 @@
+<div class="main clearfix position-relative">
+ 	<div class="main_1 clearfix position-absolute w-100">
+   		<section id="subs">
+ 			<div class="container-xl">
+  				<div class="row subs_1 bg_brown pt-4 pb-4 rounded-3 mx-0 px-2">
+   					<div class="col-md-6">
+    					<div class="subs_1l">
+	 						<h3 class="text-white">Subscribe</h3>
+	 						<p class="mb-0 text-light">Now go push your own limits and succeed!</p>
+						</div>
+   					</div>
+   					<div class="col-md-6">
+    					<div class="subs_1r">
+	  						<div class="input-group p-2 rounded-5 bg-white rounded-3">
+								<input type="text" class="form-control border-0 bg-transparent" placeholder="Enter your email">
+								<span class="input-group-btn">
+									<button class="btn btn-primary bg-transparent border-0 fs-6 col_brown" type="button">
+										<i class="fa fa-long-arrow-right"></i>
+				    				</button>
+								</span>
+							</div>
+						</div>
+   					</div>
+  				</div>
+ 			</div>
+		</section>
+ 	</div>
+ 	<div class="main_2 clearfix">
+   		<section id="footer">
+ 			<div class="footer_m p_3">
+   				<div class="container-xl">
+    				<div class="footer_1 row">
+	 					<div class="col-md-4">
+	  						<div class="footer_1i">
+	    						<div class="top_1l">
+									<h4 class="mb-0"><a class="text-white" href="index.html"><span class="span_1 text-uppercase"><i class="fa fa-graduation-cap text-white fs-2 align-middle col_brown"></i> Academy</span> <br> <span class="font_12 span_2 text-light fw-normal">University & College</span></a></h4>
+	  								<p class="text-light mt-4">The best way is to develop and follow a plan. Start with your goals in mind and then work backward to develop the plan.</p>
+	  								<h5 class="text-white">Follow Us</h5>
+	  								<ul class="social-network social-circle mb-0 mt-4">
+										<li><a href="#" class="icoRss" title="Rss"><i class="fa fa-skype"></i></a></li>
+										<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+										<li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-pinterest"></i></a></li>
+										<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+									</ul>
+								</div>
+	  						</div>
+	 					</div>
+	 					<div class="col-md-2">
+	  						<div class="footer_1i">
+	    						<h4 class="col_brown mb-4">Category</h4>
+		   						<div class="row footer_1ism">
+		 							<h6 class="fw-normal col-md-12 col-6"><a class="text-light" href="#"> Economics</a></h6>
+	 								<h6 class="fw-normal mt-2 col-md-12 col-6"><a class="text-light" href="#"> Natural</a></h6>
+	 								<h6 class="fw-normal mt-2 col-md-12 col-6"><a class="text-light" href="#"> Architecture</a></h6>
+	 								<h6 class="fw-normal mt-2 col-md-12 col-6"><a class="text-light" href="#"> Electrical</a></h6>
+	 								<h6 class="fw-normal mt-2 col-md-12 col-6"><a class="text-light" href="#"> Biological</a></h6>
+	 								<h6 class="fw-normal mt-2 col-md-12 col-6 mb-0"><a class="text-light" href="#"> Psychology</a></h6>
+	    						</div>
+	  						</div>
+	 					</div>
+	 					<div class="col-md-2">
+	 				 		<div class="footer_1i">
+	    						<h4 class="col_brown mb-4">Useful Link</h4>
+		   						<div class="row footer_1ism">
+		 							<h6 class="fw-normal col-md-12 col-6"><a class="text-light" href="#"> Home</a></h6>
+	 								<h6 class="fw-normal mt-2 col-md-12 col-6"><a class="text-light" href="#"> About</a></h6>
+	 								<h6 class="fw-normal mt-2 col-md-12 col-6"><a class="text-light" href="#"> Course</a></h6>
+	 								<h6 class="fw-normal mt-2 col-md-12 col-6"><a class="text-light" href="#"> Our Policy</a></h6>
+	 								<h6 class="fw-normal mt-2 col-md-12 col-6"><a class="text-light" href="#"> Pricing</a></h6>
+	 								<h6 class="fw-normal mt-2 col-md-12 col-6 mb-0"><a class="text-light" href="#"> Faq</a></h6>
+	    						</div>
+	  						</div>
+	 					</div>
+	 					<div class="col-md-4">
+	  						<div class="footer_1i">
+	    						<h4 class="col_brown mb-4">Contact Us</h4>
+		 						<p class="text-light">Helped thousands of clients to find the right property for their needs.</p>
+		 						<h6 class="mt-4 text-light"><i class="fa fa-map-marker col_brown me-2 fs-4 align-middle"></i> Z793 STower Suat, DB 8513 UK</h6>
+		 						<h6 class="mt-4 text-light"><i class="fa fa-microphone col_brown me-2 fs-4 align-middle"></i> +(123) 456-7890</h6>
+		 						<h6 class="mt-4 text-light mb-0"><i class="fa fa-headphones col_brown me-2 fs-4 align-middle"></i> info@gmail.com</h6>
+	  						</div>
+	 					</div>
+					</div>
+  				</div>
+ 			</div>
+		</section>
+ 	</div>
+</div>
+
+<section id="footer_b" class="bg_dark">
+ <div class="container-xl">
+    <div class="footer_b1 row text-center">
+     <div class="col-md-12">
+	   <p class="mb-0 text-light">© 2013 Your Website Name. All Rights Reserved | Design by <a class="col_brown fw-bold" href="http://www.templateonweb.com">TemplateOnWeb</a></p>
+	 </div>
+	</div>
+  </div>
+</section>
+<script>
+	window.onscroll = function() {myFunction()};
+		var navbar_sticky = document.getElementById("navbar_sticky");
+		var sticky = navbar_sticky.offsetTop;
+		var navbar_height = document.querySelector('.navbar').offsetHeight;
+		function myFunction() {
+  			if (window.pageYOffset >= sticky + navbar_height) {
+    		navbar_sticky.classList.add("sticky")
+			document.body.style.paddingTop = navbar_height + 'px';
+  		} else {
+    		navbar_sticky.classList.remove("sticky");
+			document.body.style.paddingTop = '0'
+  		}
+	}
+</script>
+
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Private Tutor Finding</title>
+	<link href="..css/bootstrap.min.css" rel="stylesheet" >
+	<link href="..css/font-awesome.min.css" rel="stylesheet" >
+	<link href="..css/header.css" rel="stylesheet">
+	<link href="..css/footer.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<script src="..js/bootstrap.bundle.min.js"></script>
+
+</head>
+<body>
+
+<section id="top" class="pt-3 pb-3">
+ <div class="container-xl">
+  <div class="row top_1">
+   <div class="col-md-4">
+    <div class="top_1l">
+	<h4 class="mb-0"><a class="col_brown" href="index.html"><span class="span_1 text-uppercase"><i class="fa fa-graduation-cap text-dark fs-2 align-middle"></i> Academy</span> <br> <span class="font_12 span_2 text-muted fw-normal">University & College</span></a></h4>
+	</div>
+   </div>
+   <div class="col-md-8">
+    <div class="top_1r">
+	   <div class="top_1ri row">
+	    <div class="col-md-4">
+		 <div class="top_1ri1">
+		  <span class="d-inline-block text-center border_1 span_1 rounded-circle float-start me-2"><i class="fa fa-phone"></i></span>
+		  <h6 class="font_14 mb-0"><span class="span_2 fw-bold d-inline-block mb-1">Call Now</span> <br> +123 4567 890</h6>
+		 </div>
+		</div>
+		<div class="col-md-4">
+		 <div class="top_1ri1">
+		  <span class="d-inline-block text-center border_1 span_1 rounded-circle float-start me-2"><i class="fa fa-envelope"></i></span>
+		  <h6 class="font_14 mb-0"><span class="span_2 fw-bold d-inline-block mb-1">Send Message</span> <br> info@gmil.com</h6>
+		 </div>
+		</div>
+		<div class="col-md-4">
+		 <div class="top_1ri1">
+		  <span class="d-inline-block text-center border_1 span_1 rounded-circle float-start me-2"><i class="fa fa-map-marker"></i></span>
+		  <h6 class="font_14 mb-0"><span class="span_2 fw-bold d-inline-block mb-1">Our Location</span> <br> 634 Semper Porta</h6>
+		 </div>
+		</div>
+	   </div>
+	</div>
+   </div>
+  </div>
+ </div>
+</section>
+
+<section id="header">
+	<nav class="navbar navbar-expand-md navbar-light bg_light p-0" id="navbar_sticky">
+  <div class="container-xl">
+    <a class="col_brown navbar-brand p-0  fw-bold me-5" href="index.html"><span class="span_1 text-uppercase"><i class="fa fa-graduation-cap text-dark fs-2 align-middle"></i> Academy</span> <br> <span class="font_12 span_2 text-muted fw-normal">University & College</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	   <ul class="navbar-nav mb-0">
+	    
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="">Home</a>
+        </li>
+		 
+		<li class="nav-item">
+          <a class="nav-link" href="">About </a>
+        </li>
+
+		<li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Blog
+          </a>
+          <ul class="dropdown-menu drop_1" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href=""><i class="fa fa-chevron-right font_8 me-1 align-middle"></i> Blog</a></li>
+            <li><a class="dropdown-item border-0" href=""><i class="fa fa-chevron-right font_8 me-1 align-middle"></i> Blog Detail</a></li>
+          </ul>
+        </li>
+		
+		<li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Courses
+          </a>
+          <ul class="dropdown-menu drop_1" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href=""><i class="fa fa-chevron-right font_8 me-1 align-middle"></i> Courses</a></li>
+            <li><a class="dropdown-item border-0" href=""><i class="fa fa-chevron-right font_8 me-1 align-middle"></i> Course Detail</a></li>
+          </ul>
+        </li>
+		
+		<li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Events
+          </a>
+          <ul class="dropdown-menu drop_1" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href=""><i class="fa fa-chevron-right font_8 me-1 align-middle"></i> Events</a></li>
+            <li><a class="dropdown-item border-0" href=""><i class="fa fa-chevron-right font_8 me-1 align-middle"></i> Event Detail</a></li>
+          </ul>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="t">Team </a>
+        </li>
+		
+		
+		<li class="nav-item">
+          <a class="nav-link" href="">Contact </a>
+        </li>
+
+      </ul>
+       <ul class="navbar-nav mb-0 ms-auto">
+		
+		<li class="nav-item">
+          <a class="button_1" href="#">Get a Quote </a>
+        </li>
+      </ul>
+    </div>
+	  </div>
+	</nav>
+</section>
