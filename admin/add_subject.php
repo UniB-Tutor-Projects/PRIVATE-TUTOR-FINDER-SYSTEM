db8e3230b28ed12103cf924ee29c5c6485a186d7
UniB-Tutor-Projects <127729570+UniB-Tutor-Projects@users.noreply.github.com>
2023-03-20 01:42:00 +0100

    Update add_subject.php

--- admin/add_subject.php
+++ admin/add_subject.php
@@ -49,7 +49,7 @@ if (isset($_POST['submit'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <title>Add TutorSubject</title>
+        <title>Add Subject</title>
     </head>
 
     <body>
@@ -59,8 +59,8 @@ if (isset($_POST['submit'])) {
 
         <div class="container">
             <div class="text-center" mb-4>
-                <h3>Add New Tutor</h3>
-                <p class="text-muted">Complete the below form to add a new Tutor!</p>
+                <h3>Add New Subject</h3>
+                <p class="text-muted">Complete the below form to add a new Subject!</p>
             </div>
 
             <div class="container d-flex justify-content-center">
