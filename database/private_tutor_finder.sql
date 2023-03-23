4ec89e609f9da9a2e0a9cbc63eb23f15e5c5376c
JuniorDCoder <119924067+JuniorDCoder@users.noreply.github.com>
2023-03-23 11:21:08 +0100

    Update private_tutor_finder.sql

--- database/private_tutor_finder.sql
+++ database/private_tutor_finder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 15, 2023 at 12:51 PM
+-- Generation Time: Mar 23, 2023 at 09:47 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -33,6 +33,13 @@ CREATE TABLE `admin` (
   `admin_password` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_name`, `admin_password`) VALUES
+(1, 'Administrator', 'admin12345');
+
 -- --------------------------------------------------------
 
 --
@@ -43,10 +50,19 @@ CREATE TABLE `appointment` (
   `appointment_id` int(11) NOT NULL,
   `tutor_id` int(11) DEFAULT NULL,
   `subject_id` int(11) DEFAULT NULL,
-  `appointment_time_start` datetime NOT NULL,
-  `appointment_time_end` datetime NOT NULL,
+  `appointment_time_start` time NOT NULL,
+  `appointment_time_end` time NOT NULL,
   `appointment_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`appointment_id`, `tutor_id`, `subject_id`, `appointment_time_start`, `appointment_time_end`, `appointment_date`) VALUES
+(1, 1, 1, '12:30:00', '14:40:00', '2023-03-24'),
+(2, 1, 3, '15:00:00', '17:00:00', '2023-03-30'),
+(3, 4, 3, '21:46:00', '22:46:00', '2023-03-30');
 
 -- --------------------------------------------------------
 
@@ -76,6 +92,14 @@ CREATE TABLE `subject` (
   `tutor_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`subject_id`, `subject_name`, `subject_code`, `tutor_id`) VALUES
+(1, 'Computational Thinking', 'COME2103', 1),
+(3, 'Data Structures', 'COME2105', 4);
+
 -- --------------------------------------------------------
 
 --
@@ -84,15 +108,25 @@ CREATE TABLE `subject` (
 
 CREATE TABLE `tutor` (
   `tutor_id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
+  `tutor_name` varchar(30) NOT NULL,
   `age` int(4) NOT NULL,
   `gender` tinytext NOT NULL,
   `certificate` varchar(256) NOT NULL,
   `subject_tutoring` varchar(256) NOT NULL,
   `tutor_phone` int(13) NOT NULL,
   `tutor_email` varchar(256) NOT NULL,
-  `tutor_address` varchar(256) NOT NULL
+  `tutor_address` varchar(256) NOT NULL,
+  `tutor_pwd` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tutor`
+--
+
+INSERT INTO `tutor` (`tutor_id`, `tutor_name`, `age`, `gender`, `certificate`, `subject_tutoring`, `tutor_phone`, `tutor_email`, `tutor_address`, `tutor_pwd`) VALUES
+(1, 'ShadowWalker', 26, 'male', 'Masters', 'Computational Thinking', 64290623, 'kundegodfrey84@gmail.com', 'Bamenda', '$2y$10$NJxCGo7Rd0XQSo5Mr5alA.gSbWLL2AfBm639XwlJ5xJs2i7JnQgk6'),
+(4, 'Edrick', 19, 'male', 'Advance Level', 'Data Structures', 677802114, 'deman@mail.com', 'Bambili', '$2y$10$yqzF/kTcka2vOEXj3YY4v.KjzhAElPH6SW//Klzvx/EQz0BdQUpiu'),
+(5, 'Ngu', 18, 'male', 'Advance Level', 'PHP Backend', 677802114, 'juniorngu84@gmail.com', 'Bambili', '$2y$10$D5MD56WpG4MzBoATTwX0FeF.sjS.aQrCTA66cLJCZyaVTcCpAbZYu');
 
 -- --------------------------------------------------------
 
@@ -107,8 +141,17 @@ CREATE TABLE `users` (
   `gender` tinytext NOT NULL,
   `phone_number` int(13) NOT NULL,
   `email` varchar(256) NOT NULL,
-  `address` varchar(256) DEFAULT NULL
+  `address` varchar(256) DEFAULT NULL,
+  `user_pwd` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `age`, `gender`, `phone_number`, `email`, `address`, `user_pwd`) VALUES
+(2, 'Junior', 19, 'male', 677802114, 'juniorngu84@gmail.com', 'Bambui-Bamenda', '$2y$10$HrNtxHwy4JOi7NdMi2rC0uFkYLg3kioaQMnNlEHc8rVqVpJ5JeKAe'),
+(5, 'UniB', 30, 'male', 689345256, 'Unib@mail.com', 'Commercial Avenue', '$2y$10$4j/C67t2KSY3Q5gIBzBdI.mADnr9eSqweSoZP8povTd0JGVH3a7Im');
 
 --
 -- Indexes for dumped tables
@@ -158,13 +201,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `appointment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `appointment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `pricing`
@@ -176,19 +219,19 @@ ALTER TABLE `pricing`
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tutor`
 --
 ALTER TABLE `tutor`
-  MODIFY `tutor_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `tutor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
